f11558c74d51ac5c2c8dce4ead1066bc5312ff14
JiayiLiuCA <liujiayi402@gmail.com>
2020-08-27 12:53:05 -0400

    background and title

---
+++ src/components/Title.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Header, Divider } from 'semantic-ui-react'
+
+
+const Title: React.FC<{}> = () => {
+    return (
+        <div className="title">
+            <Header as='h2' inverted>
+                Game of Life
+                <Header.Subheader>
+                    A turing complete cellular automaton by John Conway
+                </Header.Subheader>
+            </Header>
+            <Divider />
+        </div>
+    )
+}
+
+export default Title
