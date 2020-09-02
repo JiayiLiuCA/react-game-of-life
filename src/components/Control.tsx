e68eab8918540a177086a469291d9f5bd75ed365
JiayiLiuCA <liujiayi402@gmail.com>
2020-09-01 23:10:34 -0400

    fix typo

--- src/components/Control.tsx
+++ src/components/Control.tsx
@@ -24,7 +24,7 @@ const Control: React.FC<Prop> = ({ playing, onPlayClick, onRandomClick, onClearC
                 <Button
                     icon="redo"
                     onClick={() => onClearClick()}
-                />>
+                />
             </ButtonGroup>
         </div>
     )
