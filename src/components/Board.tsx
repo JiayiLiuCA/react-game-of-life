72be3221281ad4917c8950a4649b2df698a20156
JiayiLiuCA <liujiayi402@gmail.com>
2020-09-03 22:32:28 -0400

    Fix warning

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Control from './Control';
 import World from './World'
 
@@ -9,14 +9,14 @@ const numCol: number = 50;
 // Helper
 
 const neighbors = [
-    [-1,-1],
+    [-1, -1],
     [-1, 0],
     [-1, 1],
     [0, -1],
-    [0,  1],
+    [0, 1],
     [1, -1],
-    [1,  0],
-    [1,  1]
+    [1, 0],
+    [1, 1]
 ]
 
 
@@ -46,22 +46,10 @@ const Board: React.FC = () => {
     const [generation, setGeneration] = useState(0);
     const [playing, setPlaying] = useState(false);
 
-    
-    useEffect(() => {
-        console.log("effect")
-        if (!playing) {
-            return;
-        }
-        const interval = setInterval(() => {
-            nextGen();
-            setGeneration(prevGen => prevGen+1);
-        }, 10)
-        return () => {
-            clearInterval(interval);
-        }
-    }, [playing,generation])
 
-    const onGridClick = (row:number, col:number):void => {
+    
+
+    const onGridClick = (row: number, col: number): void => {
         let newRow = [...world[row]];
         newRow[col] = world[row][col] ? 0 : 1;
         let newWorld = [...world];
@@ -69,19 +57,19 @@ const Board: React.FC = () => {
         setWorld(newWorld);
     }
 
-    const nextGen = ():void => {
+    const nextGen = useCallback((): void => {
         let newWorld = generateEmptyWorld();
-        for(let i = 0; i<numRow; i++) {
-            for(let j = 0; j<numCol; j++) {
+        for (let i = 0; i < numRow; i++) {
+            for (let j = 0; j < numCol; j++) {
                 let countLiveNeighbors = 0
-                neighbors.forEach(([x,y]) => {
-                    const newX = x+i;
-                    const newY = y+j;
+                neighbors.forEach(([x, y]) => {
+                    const newX = x + i;
+                    const newY = y + j;
                     if (newX >= 0 && newX < numRow && newY >= 0 && newY < numCol) {
                         countLiveNeighbors += world[newX][newY];
                     }
                 });
-                
+
                 if (countLiveNeighbors < 2 || countLiveNeighbors > 3) {
                     newWorld[i][j] = 0;
                 }
@@ -94,11 +82,26 @@ const Board: React.FC = () => {
             }
         }
         setWorld(newWorld);
-    }
+    }, [world]);
+
+    useEffect(() => {
+        console.log("effect")
+        if (!playing) {
+            return;
+        }
+        const interval = setInterval(() => {
+            nextGen();
+            setGeneration(prevGen => prevGen + 1);
+        }, 10)
+        return () => {
+            clearInterval(interval);
+        }
+    }, [playing, generation, nextGen])
+
 
     const onPlay = (): void => {
         setPlaying(!playing);
-        
+
     }
 
     const onRandom = (): void => {
@@ -113,12 +116,12 @@ const Board: React.FC = () => {
     return (
         // board
         <div className="board">
-            <World world={world} onGridClick={onGridClick}/>
+            <World world={world} onGridClick={onGridClick} />
             <Control
                 playing={playing}
                 onPlay={onPlay}
                 nextGen={nextGen}
-                onRandom={onRandom} 
+                onRandom={onRandom}
                 onClear={onClear}
             />
         </div>
